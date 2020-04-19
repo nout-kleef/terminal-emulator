7fe7e8e15727371a78b8020b42a980d872613841
Nout Kleef <noutkleef1@gmail.com>
2020-04-19 16:08:47 +0200

    no more sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ const src = "./src",
 const gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	del = require("del"),
-	sourcemaps = require("gulp-sourcemaps"),
 	less = require("gulp-less"),
 	concat = require("gulp-concat"),
 	cleanCSS = require("gulp-clean-css"),
@@ -19,13 +18,10 @@ const gulp = require('gulp'),
 	});
 
 // TASK COMPOSITIONS
-// builds assets
 const generate = gulp.parallel(compileStyle, compileJs, moveHtml);
-// cleans, then builds
 const build = gulp.series(clean, generate);
 
 // TASK MODULES
-// error reporting
 function logError(error) {
 	gutil.log(
 		gutil.colors.red('\n!! !! !!\n!! !! !!\n!! !! !!'),
@@ -33,50 +29,36 @@ function logError(error) {
 	);
 	gutil.log(gutil.colors.red('[Error]'), error.toString());
 }
-// cleanup
 function clean(cb) {
-	// delete all existing assets and html to prevent caching issues
 	del([
 		dist + "*/" + subs + ".*", // empty any subdirectory
 		dist + "*.*"
-		/* delete all files that arent directories
-			(*.* can theoretically still be a directory. I know.) */
 	]);
 	cb();
 }
-// html
 function moveHtml(cb) {
 	gulp.src(src + subs + ".html").on("error", logError)
 		.pipe(gulp.dest(dist)).on("error", logError);
 	cb();
 }
-// less/css
 function compileStyle(cb) {
 	gulp.src(src + "/less/main.less").on("error", logError)
-		.pipe(sourcemaps.init())
-		.pipe(less( /*{plugins: [autoprefix]}*/ )).on("error", logError)
+		.pipe(less( /*{plugins: [autoprefix]}*/)).on("error", logError)
 		.pipe(concat("terminal-emulator.css")).on("error", logError)
-		.pipe(sourcemaps.write())
 		.pipe(gulp.dest(dist));
 	cb();
 }
-// javascript
 function compileJs(cb) {
-	// JQuery (not actually included, just there for demo page)
 	gulp.src(node + "jquery/dist/jquery.slim.js") // slim: no extensions
 		.pipe(gulp.dest(dist)).on("error", logError);
-	// demo script, implementing example usage
 	gulp.src(src + "/js/demo/emulator.js")
 		.pipe(gulp.dest(dist)).on("error", logError);
 	gulp.src([src + "/js/*.js"]).on("error", logError)
-		.pipe(sourcemaps.init())
 		.pipe(concat("terminal-emulator.min.js")).on("error", logError)
 		.pipe(uglify()).on("error", logError)
-		.pipe(sourcemaps.write())
 		.pipe(gulp.dest(dist)).on("error", logError);
 	cb();
 }
-// exports
 exports.clean = clean;
 exports.generate = generate;
 exports.default = build;
